6569c7351debe8bb59dd2927d82f61b58d446c87
shamalka <shamalkanavod95@gmail.com>
2018-09-06 10:59:44 +0530

    Project files

---
+++ TrainTracking2/app/src/main/java/com/snov/traintracking/utilities/Constants.java
@@ -0,0 +1,12 @@
+package com.snov.traintracking.utilities;
+
+import java.io.Serializable;
+
+/**
+ * Created by Shamalka Navod on 2018-08-30.
+ */
+public class Constants implements Serializable {
+    //server url
+    public static String SERVER_URL = "http://192.168.8.101/RailwaySystem/api.php/";
+    public static String FIREBASE_DATABASE_URL = "https://train-tracking-app.firebaseio.com/";
+}
